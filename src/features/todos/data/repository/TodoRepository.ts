d05d1f48863fb9cbaba1c270952d8af134ee41d5
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-07-24 16:32:18 +0600

    📚 Refine TodoRepository interface return types

--- src/features/todos/data/repository/TodoRepository.ts
+++ src/features/todos/data/repository/TodoRepository.ts
@@ -1,9 +1,11 @@
 import type { Todo } from "@/features/todos/domain/entities/Todo";
+import type { ApiResponse } from "@/shared/types/api-response.type";
+import type { PaginatedResponse } from "@/shared/types/paginated-response.type";
 
 export interface TodoRepository {
-  getTodos(): Promise<Todo[]>;
-  createTodo(todo: { title: string }): Promise<Todo>;
-  updateTodo(id: string, todo: { title: string }): Promise<Todo>;
-  updateTodoStatus(id: string, completed: boolean): Promise<Todo>;
-  deleteTodo(id: string): Promise<void>;
+  getTodos(): Promise<ApiResponse<PaginatedResponse<Todo>>>;
+  createTodo(todo: { title: string }): Promise<ApiResponse<Todo>>;
+  updateTodo(id: string, todo: { title: string }): Promise<ApiResponse<Todo>>;
+  updateTodoStatus(id: string, completed: boolean): Promise<ApiResponse<Todo>>;
+  deleteTodo(id: string): Promise<ApiResponse<void>>;
 }
