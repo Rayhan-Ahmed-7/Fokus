b117f8794fcbcd15a5cefc2a63d301dd40df747c
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-07-24 16:32:18 +0600

    🏗️ Update TodoRepositoryImpl to use refined types

--- src/features/todos/data/repository/TodoRepositoryImpl.ts
+++ src/features/todos/data/repository/TodoRepositoryImpl.ts
@@ -1,6 +1,8 @@
+import type { ApiResponse } from "@/shared/types/api-response.type";
 import type { Todo } from "../../domain/entities/Todo";
 import type { TodoDataSource } from "../dataSource/types/TodoDataSource";
 import type { TodoRepository } from "./TodoRepository";
+import type { PaginatedResponse } from "@/shared/types/paginated-response.type";
 
 export class TodoRepositoryImpl implements TodoRepository {
   private dataSource: TodoDataSource;
@@ -8,23 +10,23 @@ export class TodoRepositoryImpl implements TodoRepository {
     this.dataSource = dataSource;
   }
 
-  async getTodos(): Promise<Todo[]> {
+  async getTodos(): Promise<ApiResponse<PaginatedResponse<Todo>>> {
     return this.dataSource.fetchTodos();
   }
 
-  async createTodo(todo: { title: string }): Promise<Todo> {
+  async createTodo(todo: { title: string }): Promise<ApiResponse<Todo>> {
     return this.dataSource.createTodo(todo);
   }
 
-  async updateTodo(id: string, todo: { title: string }): Promise<Todo> {
+  async updateTodo(id: string, todo: { title: string }): Promise<ApiResponse<Todo>> {
     return this.dataSource.updateTodo(id, todo);
   }
 
-  async updateTodoStatus(id: string, completed: boolean): Promise<Todo> {
+  async updateTodoStatus(id: string, completed: boolean): Promise<ApiResponse<Todo>> {
     return this.dataSource.updateTodoStatus(id, completed);
   }
 
-  async deleteTodo(id: string): Promise<void> {
+  async deleteTodo(id: string): Promise<ApiResponse<void>> {
     return this.dataSource.deleteTodo(id);
   }
 }
