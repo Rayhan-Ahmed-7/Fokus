61cd8c390340468fd5324278ebec2533479b0121
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-07-30 10:47:17 +0600

    :test_tube: sonar qube properties added & vitest test coverage added

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
@@ -12,5 +12,9 @@ export default defineConfig({
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     environment: "jsdom",
     globals: true,
+    coverage: {
+      reporter: ["text", "lcov"],
+      reportsDirectory: "./coverage",
+    },
   },
 });
