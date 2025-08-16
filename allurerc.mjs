277c0fc792c0231266dfebe62152d533c00c8cb9
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-08-16 20:27:08 +0600

    :bug: allure report generation issue fixed

--- allurerc.mjs
+++ allurerc.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "allure";
 export default defineConfig({
   name: "Fokus Test Report",
   output: "./allure-report",
-  historyPath: "./history.jsonl",
+  historyPath: "./history.json",
   plugins: {
     awesome: {
       options: {
