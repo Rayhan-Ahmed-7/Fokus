68f7d19eb2038e04165d237dfcf4afa926ccd39a
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-08-17 14:03:53 +0600

    :art: badge variant added

--- src/stories/Badge.stories.tsx
+++ src/stories/Badge.stories.tsx
@@ -17,6 +17,10 @@ export const Variants: Story = {
       <Badge variant="secondary">Secondary</Badge>
       <Badge variant="destructive">Destructive</Badge>
       <Badge variant="outline">Outline</Badge>
+      <Badge variant="success">Success</Badge>
+      <Badge variant="warning">Warning</Badge>
+      <Badge variant="info">Info</Badge>
+      <Badge variant="error">Error</Badge>
     </div>
   ),
 };
