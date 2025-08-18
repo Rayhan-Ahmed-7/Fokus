16e4045216047dda014f9f50618b77045394cb3a
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-08-18 11:03:22 +0600

    :art: Update AppSidebar UI/UX

--- src/core/layout/AppSidebar.tsx
+++ src/core/layout/AppSidebar.tsx
@@ -5,17 +5,19 @@ import {
   SidebarHeader,
 } from "@/components/ui/sidebar";
 import ThemeSwitcher from "./ThemeSwitcher";
-import { Target } from "lucide-react";
 import { menuData } from "./menu";
 import { renderMenu } from "./renderMenu";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 const AppSidebar = () => {
   return (
     <Sidebar className="h-full">
       {/* Header */}
       <SidebarHeader className="flex items-center gap-2 px-4 py-3 border-b">
-        <Target className="w-6 h-6 text-primary" />
-        <span className="text-lg font-bold tracking-tight">Fokus</span>
+        <Avatar className="w-12 h-12">
+          <AvatarImage src="/fav.png" />
+        </Avatar>
+        <span className="text-xl tracking-widest font-bold">Fokus App</span>
       </SidebarHeader>
       {/* Content */}
       <SidebarContent className="overflow-auto">
