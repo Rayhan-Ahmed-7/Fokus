f1ea6a5fe2136c56fe029bb7be41f521094d5372
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-01 20:28:57 +0600

    :page_facing_up: Update TodoPage view

--- src/features/todos/presentation/view/TodoPage.tsx
+++ src/features/todos/presentation/view/TodoPage.tsx
@@ -5,7 +5,7 @@ import TodoList from "./TodoList";
 const TodoPage = () => {
   const { t } = useTranslation("todos");
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 p-6">
       <h1 className="text-2xl font-bold">{t("title")}</h1>
       <CreateTodo />
       <TodoList />
