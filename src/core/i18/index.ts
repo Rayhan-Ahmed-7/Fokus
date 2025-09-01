eb50dcacefbc114a7b9f6a88cce847e8b109b4b6
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-01 18:12:13 +0600

    :globe_with_meridians: Update i18n index with new languages and namespaces

--- src/core/i18/index.ts
+++ src/core/i18/index.ts
@@ -2,22 +2,34 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enCommon from "./locales/en/common.json";
 import deCommon from "./locales/de/common.json";
+import bnCommon from "./locales/bn/common.json";
 import enMenu from "./locales/en/menu.json";
 import deMenu from "./locales/de/menu.json";
+import bnMenu from "./locales/bn/menu.json";
+import enTodos from "@/features/todos/locales/en.json";
+import deTodos from "@/features/todos/locales/de.json";
+import bnTodos from "@/features/todos/locales/bn.json";
 i18n.use(initReactI18next).init({
   resources: {
     en: {
       menu: enMenu,
       common: enCommon,
+      todos: enTodos,
     },
     de: {
       menu: deMenu,
       common: deCommon,
+      todos: deTodos,
+    },
+    bn: {
+      menu: bnMenu,
+      common: bnCommon,
+      todos: bnTodos,
     },
   },
   lng: "en", // default language
   fallbackLng: "en",
-  ns: ["menu", "common"], // namespaces (can extend with feature-based)
+  ns: ["menu", "common", "todos"], // namespaces (can extend with feature-based)
   interpolation: {
     escapeValue: false,
   },
