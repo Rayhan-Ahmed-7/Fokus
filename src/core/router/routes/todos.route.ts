986278ccba80cfb7e90ad2df990323abb43c9659
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-01 20:28:50 +0600

    :memo: Add/update todos route with staticData breadcrumb

--- src/core/router/routes/todos.route.ts
+++ src/core/router/routes/todos.route.ts
@@ -1,10 +1,11 @@
 import { createRoute } from "@tanstack/react-router";
 import { rootRoute } from "./root.route";
 import TodoPage from "@/features/todos/presentation/view/TodoPage";
-import { breadcrumbLoader } from "@/core/utils/breadcrumb";
 export const todosRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "todos",
   component: TodoPage,
-  loader: breadcrumbLoader("todos"),
+  staticData: {
+    breadcrumb: "todos",
+  },
 });
