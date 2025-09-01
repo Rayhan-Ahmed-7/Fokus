3f7aeb769fbb2f921b37d2b5c3f8f7efe3e1358a
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-01 18:12:26 +0600

    :memo: Add meta breadcrumb key to Todos route

--- src/core/router/routes/todos.route.ts
+++ src/core/router/routes/todos.route.ts
@@ -1,8 +1,10 @@
 import { createRoute } from "@tanstack/react-router";
 import { rootRoute } from "./root.route";
 import TodoPage from "@/features/todos/presentation/view/TodoPage";
+import { breadcrumbLoader } from "@/core/utils/breadcrumb";
 export const todosRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "todos",
   component: TodoPage,
+  loader: breadcrumbLoader("todos"),
 });
