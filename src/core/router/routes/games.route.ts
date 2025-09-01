d2e4f0f3699e7d237592acf124d993e43a8ca615
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-01 21:04:08 +0600

    :video_game: Update games route

--- src/core/router/routes/games.route.ts
+++ src/core/router/routes/games.route.ts
@@ -5,10 +5,16 @@ import TicTacToePage from "@/features/games/tictactoe/view/TicTacToePage";
 export const gamesRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "games",
+  staticData: {
+    breadcrumb: "games",
+  },
 });
 
 export const ticTacToeRoute = createRoute({
   getParentRoute: () => gamesRoute,
   path: "tictactoe",
   component: TicTacToePage,
+  staticData: {
+    breadcrumb: "games.tictactoe",
+  },
 });
