2e2465bda9d05e31c1cf96c5647854e3e9e4934e
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-14 23:16:34 +0600

    :zap: Update i18n initialization for Arabic language

--- src/core/i18/index.ts
+++ src/core/i18/index.ts
@@ -3,19 +3,31 @@ import { initReactI18next } from "react-i18next";
 import enCommon from "./locales/en/common.json";
 import deCommon from "./locales/de/common.json";
 import bnCommon from "./locales/bn/common.json";
+import arCommon from "./locales/ar/common.json";
+
 import enMenu from "./locales/en/menu.json";
 import deMenu from "./locales/de/menu.json";
 import bnMenu from "./locales/bn/menu.json";
+import arMenu from "./locales/ar/menu.json";
+
 import enTodos from "@/features/todos/locales/en.json";
 import deTodos from "@/features/todos/locales/de.json";
 import bnTodos from "@/features/todos/locales/bn.json";
+import arTodos from "@/features/todos/locales/ar.json";
 
 import enHome from "@/features/home/locales/en.json";
 import deHome from "@/features/home/locales/de.json";
 import bnHome from "@/features/home/locales/bn.json";
+import arHome from "@/features/home/locales/ar.json";
 
 i18n.use(initReactI18next).init({
   resources: {
+    ar: {
+      menu: arMenu,
+      common: arCommon,
+      home: arHome,
+      todos: arTodos,
+    },
     en: {
       menu: enMenu,
       common: enCommon,
