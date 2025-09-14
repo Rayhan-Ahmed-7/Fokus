ab2a7df39b450fd7a755021dc482929451f67bf8
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-14 23:16:27 +0600

    :zap: Add flags index file

---
+++ src/assets/flags/index.ts
@@ -0,0 +1,6 @@
+import EnglishFlag from "@/assets/flags/united-kingdom.svg?react";
+import GermanFlag from "@/assets/flags/germany.svg?react";
+import BengaliFlag from "@/assets/flags/bangladesh.svg?react";
+import SaudiArabFlag from "@/assets/flags/saudi-arabia.svg?react";
+
+export { EnglishFlag, GermanFlag, BengaliFlag, SaudiArabFlag };
