a7706a6ff7e7737e14b3d320520dd70a2213727c
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-16 00:12:09 +0600

    :recycle: Refactor i18next setup and locale resources

--- src/core/i18/index.ts
+++ src/core/i18/index.ts
@@ -1,61 +1,22 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import enCommon from "./locales/en/common.json";
-import deCommon from "./locales/de/common.json";
-import bnCommon from "./locales/bn/common.json";
-import arCommon from "./locales/ar/common.json";
+import ICU from "i18next-icu";
 
-import enMenu from "./locales/en/menu.json";
-import deMenu from "./locales/de/menu.json";
-import bnMenu from "./locales/bn/menu.json";
-import arMenu from "./locales/ar/menu.json";
-
-import enTodos from "@/features/todos/locales/en.json";
-import deTodos from "@/features/todos/locales/de.json";
-import bnTodos from "@/features/todos/locales/bn.json";
-import arTodos from "@/features/todos/locales/ar.json";
-
-import enHome from "@/features/home/locales/en.json";
-import deHome from "@/features/home/locales/de.json";
-import bnHome from "@/features/home/locales/bn.json";
-import arHome from "@/features/home/locales/ar.json";
 import { store } from "../store";
+import { resources } from "./locales";
 
 const initialLang = store.getState().theme.language || "ar";
-console.log("Initial Language:", initialLang);
-i18n.use(initReactI18next).init({
-  resources: {
-    ar: {
-      menu: arMenu,
-      common: arCommon,
-      home: arHome,
-      todos: arTodos,
+i18n
+  .use(initReactI18next)
+  .use(ICU)
+  .init({
+    resources: resources,
+    lng: initialLang, // default language
+    fallbackLng: "en",
+    ns: ["menu", "common", "todos"], // namespaces (can extend with feature-based)
+    interpolation: {
+      escapeValue: false,
     },
-    en: {
-      menu: enMenu,
-      common: enCommon,
-      home: enHome,
-      todos: enTodos,
-    },
-    de: {
-      menu: deMenu,
-      common: deCommon,
-      home: deHome,
-      todos: deTodos,
-    },
-    bn: {
-      menu: bnMenu,
-      common: bnCommon,
-      home: bnHome,
-      todos: bnTodos,
-    },
-  },
-  lng: initialLang, // default language
-  fallbackLng: "en",
-  ns: ["menu", "common", "todos"], // namespaces (can extend with feature-based)
-  interpolation: {
-    escapeValue: false,
-  },
-});
+  });
 
 export default i18n;
