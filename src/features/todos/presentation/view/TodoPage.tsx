0b1dc238f4faf028cb00c466a950a248d4d6d942
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-16 00:12:17 +0600

    :recycle: Refactor TodoPage to use i18n for numbers and currency

--- src/features/todos/presentation/view/TodoPage.tsx
+++ src/features/todos/presentation/view/TodoPage.tsx
@@ -3,10 +3,12 @@ import { CreateTodo } from "./CreateTodo";
 import TodoList from "./TodoList";
 
 const TodoPage = () => {
-  const { t } = useTranslation("todos");
+  const { t } = useTranslation();
   return (
     <div className="space-y-6 p-6">
-      <h1 className="text-2xl font-bold">{t("title")}</h1>
+      <h1 className="text-2xl font-bold">{t("todos:title")}</h1>
+      {t("common:currency", { amount: 1234.56 })}
+      {t("common:number", { amount: 1234.56, locale: "ar-SA" })}
       <CreateTodo />
       <TodoList />
     </div>
