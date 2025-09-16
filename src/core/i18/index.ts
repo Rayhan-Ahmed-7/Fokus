25aee41d249384c1a3cec4f5b4dc64131b92711c
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-09-16 10:10:33 +0600

    :globe_with_meridians: Adjust i18n config for Saudi locale fallback

--- src/core/i18/index.ts
+++ src/core/i18/index.ts
@@ -5,14 +5,16 @@ import ICU from "i18next-icu";
 import { store } from "../store";
 import { resources } from "./locales";
 
-const initialLang = store.getState().theme.language || "ar";
+const initialLang = store.getState().theme.language || "en";
 i18n
   .use(initReactI18next)
   .use(ICU)
   .init({
     resources: resources,
     lng: initialLang, // default language
-    fallbackLng: "en",
+    fallbackLng: {
+      default: ["en"],
+    },
     ns: ["menu", "common", "todos"], // namespaces (can extend with feature-based)
     interpolation: {
       escapeValue: false,
