0be675d8c24244d250fc9b6b623987e514787753
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-10-09 20:55:01 +0600

    :globe_with_meridians: Update Japanese common translations

--- src/components/CommingSoon/CommingSoon.tsx
+++ src/components/CommingSoon/CommingSoon.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { Button } from "../ui/button";
+import { useTranslation } from "react-i18next";
 
 const ComingSoon = () => {
+  const { t } = useTranslation("common");
   const textRef = useRef<HTMLHeadingElement>(null);
 
   useEffect(() => {
@@ -19,23 +20,18 @@ const ComingSoon = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full text-center px-4">
       <Avatar className="w-50 h-50 p-4 sm:w-40 sm:h-40 sm:p-0 lg:w-82 lg:h-82 lg:p-2">
-        <AvatarImage src="/happy-coder.png" />
-        <AvatarFallback>😊</AvatarFallback>
+        <AvatarImage className=" rounded-none" src="/happy-coder.png" />
+        <AvatarFallback className="w-50 h-50 p-4 sm:w-40 sm:h-40 sm:p-0 lg:w-82 lg:h-82 lg:p-2">
+          😊
+        </AvatarFallback>
       </Avatar>
 
       <h1
         ref={textRef}
         className="text-2xl sm:text-3xl lg:text-5xl font-bold text-foreground mb-4 sm:mb-6"
       >
-        Coming Soon!
+        {t("commingSoon")}
       </h1>
-
-      <Button
-        onClick={() => console.log("Redirect to another page")}
-        // className="px-4 py-2 sm:px-5 sm:py-3 lg:px-6 lg:py-3 rounded-lg shadow-md text-primary-foreground"
-      >
-        Notify Me
-      </Button>
     </div>
   );
 };
