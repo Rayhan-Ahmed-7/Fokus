5dbf0d797afc3e73786f5ffb1aa28f37e255a06d
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-10-15 14:12:07 +0600

    :zap: home page image changed

--- src/features/todos/presentation/view/TodoPage.tsx
+++ src/features/todos/presentation/view/TodoPage.tsx
@@ -7,7 +7,7 @@ const TodoPage = () => {
   return (
     <div className="space-y-6 p-6">
       <h1 className="text-2xl font-bold">{t("todos:title")}</h1>
-      <p>Local Number : {t("common:currency", { amount: 1234.56 })}</p>
+      {/* <p>Local Number : {t("common:currency", { amount: 1234.56 })}</p> */}
       <CreateTodo />
       <TodoList />
     </div>
