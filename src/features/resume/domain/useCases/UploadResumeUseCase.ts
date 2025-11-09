a87259b8b76675478bead32541bd346f04ff3398
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-11-09 08:42:12 +0600

    :rocket: Enhance UploadResumeUseCase to integrate with new repository layer

--- src/features/resume/domain/useCases/UploadResumeUseCase.ts
+++ src/features/resume/domain/useCases/UploadResumeUseCase.ts
@@ -1,9 +1,10 @@
 import type { IResumeRepository } from "@/features/resume/data/repository/IResumeRepository";
+import type { UploadResponse } from "../types/resume.types";
 
 export class UploadResumeUseCase {
   constructor(private repository: IResumeRepository) {}
 
-  async execute(file: File) {
+  async execute(file: File): Promise<UploadResponse> {
     return this.repository.upload(file);
   }
 }
